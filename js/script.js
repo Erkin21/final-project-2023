08e610a6ea8456373adcb282d8bb78aabab40f0e
Erkin Abdullaev <abdullaeverkin1@gmail.com>
2023-07-17 16:57:42 -0400

    intial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Nav Bar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
+
+
+
+
+
