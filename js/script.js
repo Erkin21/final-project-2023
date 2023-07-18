9415f6d98eebc01546b3a85f7c807a83d85f5a02
Erkin Abdullaev <abdullaeverkin1@gmail.com>
2023-07-18 18:55:07 -0400

    "7/18/2023"

--- js/script.js
+++ js/script.js
@@ -2,14 +2,8 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-      x.className += " responsive";
+    x.className += " responsive";
   } else {
-      x.className = "topnav";
+    x.className = "topnav";
   }
 }
-
-
-
-
-
-
