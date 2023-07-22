614b9165f248ad9e7aca6e9d1b06539133358a47
Tafhim Khan <tafhimkhan@Tafhims-MacBook-Pro-2.local>
2023-07-22 12:33:47 -0400

    initial commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+// Search
+function searchGames() {
+  let input = document.getElementById("searchInput").value;
+  let slideshows = document.querySelectorAll(".slideshow-container");
+
+  for (let i = 0; i < slideshows.length; i++) {
+    const gameTitle = slideshows[i]
+      .querySelector(".gameTitle")
+      .innerText.toLowerCase();
+    const gameSummary = slideshows[i]
+      .querySelector(".summary")
+      .innerText.toLowerCase();
+    const shouldDisplay =
+      gameTitle.includes(input.toLowerCase()) ||
+      gameSummary.includes(input.toLowerCase());
+
+    slideshows[i].style.display = shouldDisplay ? "" : "none";
+  }
+}
+
+document.getElementById("searchInput").addEventListener("input", searchGames);
+
